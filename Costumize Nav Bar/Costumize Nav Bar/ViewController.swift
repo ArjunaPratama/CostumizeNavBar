a688d7d510ac783b0d5e05195d0e9897cc6bd432
ArjunaPratama <32661531+ArjunaPratama@users.noreply.github.com>
2017-10-10 11:37:29 +0700

    Add navigation and change color and add image

--- Costumize Nav Bar/Costumize Nav Bar/ViewController.swift
+++ Costumize Nav Bar/Costumize Nav Bar/ViewController.swift
@@ -20,6 +20,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //add function did appear 
+    override func viewDidAppear(_ animated: Bool) {
+        <#code#>
+        // deklarasi nav sebagai navigation bar 
+        let nav = self .navigationController?.navigationBar
+        
+        //tambahkan style bar 
+        nav?.barStyle = UIBarStyle.black
+        nav?.tintColor = UIColor.yellow
+        
+        //menambahkan nama gambarnya 
+        let imageview = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        imageview.contentMode = .scaleAspectFill
+        
+        //memanggil method nama gambarnya 
+        let image = UIImage(named: "hotel.png")
+        //menampilkan image ke image view 
+        imageview.image = image
+        
+        //menampilkan image ke imageview 
+        imageview.image = image 
+    }
 
 }
 
